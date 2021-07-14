5af01e072f33206d8f852d1b1f44bc3ff038a986
liza <pl.1.el.vas@gmail.com>
2021-07-14 15:04:11 +0300

    jws auth 1

--- TradingPlatform/settings.py
+++ TradingPlatform/settings.py
@@ -69,27 +69,14 @@ TEMPLATES = [
         },
     },
 ]
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.BasicAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#     ],
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.AllowAny',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 WSGI_APPLICATION = 'TradingPlatform.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# #
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
--- trading/urls.py
+++ trading/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('', ),
+    path('hello/', views.HelloView.as_view(), name='hello'),
 ]
