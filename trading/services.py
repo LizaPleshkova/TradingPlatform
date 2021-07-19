daffe7ceb24c7b7b15d2db4ad7cf4fbdeb91e18f
liza <Liza@DESKTOP-0RR514H>
2021-07-19 21:22:38 +0300

    updating serializer(valid)\views\service

--- trading/services.py
+++ trading/services.py
@@ -1,73 +1,42 @@
 from decimal import Decimal
 
-from django.core.serializers import get_serializer
-from django.http import Http404
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from requests import Response
-from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
-from .serializers import OfferListSerializer, ItemSerializer, WatchListSerializer, InventorySerializer, \
+from .serializers import OfferListSerializer, ItemSerializer, WatchListSerializer, \
     CurrencySerializer, PriceSerializer, TradeSerializer
 from .models import Currency, Item, Price, WatchList, Offer, Trade, Inventory, UserProfile, OfferCnoice
 
+
 class BaseService:
 
-    @staticmethod
-    def get_validate_data(request):
-        serializer = OfferListSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return serializer, serializer.validated_data
-
-    @staticmethod
-    def get_validate_data_trade(request):
-        serializer = TradeSerializer(data=request.data)
+    def get_validate_data(self, request):
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return serializer, serializer.validated_data
 
 
 class OfferService(BaseService):
 
-    @staticmethod
-    def get_validate_data(request):
+    def get_validate_data(self, request):
         request.data['user'] = request.user.id
-        return super(OfferService, OfferService).get_validate_data(request)
-        #        {
-        #     'type_transaction': serializer.validated_data.get('type_transaction'),
-        #     'item': serializer.validated_data.get('type_transaction'),
-        #     'user': self.request.user.id,
-        #     'price': serializer.validated_data.get('price'),
-        #     'quantity': serializer.validated_data.get('quantity'),
-        # }
-
-    @staticmethod
-    def checking_quantity_stocks_seller(data):
-        # if data.get('type_transaction') == OfferCnoice.SELL:
-        if data.get('type_transaction') == 'SELL':
-            try:
-                inventory_seller = Inventory.objects.get(user=data.get('user'),
-                                                         item=data.get('item'))
-                if inventory_seller.quantity < data.get('quantity'):
-                    # return Response({'You want to sell more stocks than you have'}, status=status.HTTP_400_BAD_REQUEST)
-                    raise ValueError('You want to sell more stocks than you have')
-            except Inventory.DoesNotExist:
-                # return Response({"No Inventory seller matches the given query."}, status=status.HTTP_400_BAD_REQUEST)
-                raise ObjectDoesNotExist('No Inventory seller matches the given query')
+        return super(OfferService, self).get_validate_data(request)
 
 
 class TradeService:
 
     @staticmethod
-    def updating_user_score(user_id, user_offer, buyer_offer=None):
+    def updating_user_score(user_id: int, user_offer: Offer, buyer_offer=None):
         user_profile = get_object_or_404(UserProfile, user_id=user_id)
-        if user_offer.type_transaction == "BUY":
+        if user_offer.type_transaction == OfferCnoice.BUY:
             user_profile.score = user_profile.score - user_offer.price * user_offer.quantity
-        elif user_offer.type_transaction == "SELL":
+        elif user_offer.type_transaction == OfferCnoice.SELL:
             user_profile.score = user_profile.score + buyer_offer.price * buyer_offer.quantity
         user_profile.save(update_fields=["score"])
         return user_profile
 
     @staticmethod
-    def updating_inventory_buyer(user, user_offer):
+    def updating_inventory_buyer(user: User, user_offer: Offer):
         inventory_buyer, created = Inventory.objects.get_or_create(user=user, item=user_offer.item)
         if created:
             inventory_buyer.quantity = user_offer.quantity
@@ -77,19 +46,17 @@ class TradeService:
         return inventory_buyer
 
     @staticmethod
-    def updating_inventory_seller(user, user_offer, buyer_offer):
+    def updating_inventory_seller(user: User, user_offer: Offer, buyer_offer: Offer):
         try:
-            # inventory_seller = get_object_or_404(Inventory, user=user, item=user_offer.item)
             inventory_seller = Inventory.objects.get(user=user, item=user_offer.item)
             inventory_seller.quantity -= buyer_offer.quantity
             inventory_seller.save(update_fields=["quantity"])
             return inventory_seller
-
         except Inventory.DoesNotExist:
             raise ObjectDoesNotExist("No Inventory seller matches the given query.")
 
     @staticmethod
-    def updating_price_item(buyer_offer):
+    def updating_price_item(buyer_offer: Offer):
         price_item = buyer_offer.item.item_price.get()
         price_item.price += Decimal(buyer_offer.price) * Decimal(0.01)
         price_item.save(update_fields=["price"])
--- trading/views.py
+++ trading/views.py
@@ -6,34 +6,35 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions, viewsets, status
 from rest_framework.response import Response
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
-from .serializers import OfferListSerializer, ItemSerializer, WatchListSerializer, InventorySerializer, \
-    CurrencySerializer, PriceSerializer, TradeSerializer
+
+from . import serializers
+from .serializers import OfferListSerializer, ItemSerializer, WatchListSerializer, \
+    CurrencySerializer, PriceSerializer, TradeSerializer, OfferDetailSerializer, ItemDetailSerializer, \
+    TradeDetailSerializer, InventoryDetailSerializer, \
+    InventorySerializer
 from .models import Currency, Item, Price, WatchList, Offer, Trade, Inventory, UserProfile
-from rest_framework import generics
-from decimal import Decimal
 from .services import TradeService, OfferService, BaseService
 
 User = get_user_model()
 
 
-# class OfferList(ListModelMixin, RetrieveModelMixin, CreateModelMixin, generics.GenericAPIView):
-#     permission_classes = (IsAuthenticated,)
-#     serializer_class = OfferListSerializer
-#     queryset = Offer.objects.all()
-#
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request)
-#
-
 class OfferListUserView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet, OfferService):
     permission_classes = (IsAuthenticated,)
-    serializer_class = OfferListSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Offer.objects.filter(user=self.request.user)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' or self.action == 'create':
+            return OfferDetailSerializer
+        return OfferListSerializer
 
     def create(self, request, *args, **kwargs):
         try:
-            serializer, offer_data = OfferService.get_validate_data(request)
-            OfferService.checking_quantity_stocks_seller(offer_data)
-        except ValueError:
+            serializer, offer_data = self.get_validate_data(request)
+            # OfferService.checking_quantity_stocks_seller(offer_data)
+        except serializers.ValidationError:
             return Response({'You want to sell more stocks than you have'}, status=status.HTTP_400_BAD_REQUEST)
         except ObjectDoesNotExist:
             return Response({"No Inventory seller matches the given query."}, status=status.HTTP_400_BAD_REQUEST)
@@ -42,15 +43,15 @@ class OfferListUserView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, vi
             headers = self.get_success_headers(offer_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def get_queryset(self, *args, **kwargs):
-        queryset = Offer.objects.filter(user=self.request.user)
-        return queryset
-
 
 class ItemView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (IsAuthenticated,)
-    serializer_class = ItemSerializer
     queryset = Item.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' or self.action == 'create':
+            return ItemDetailSerializer
+        return ItemSerializer
 
 
 class WatchListView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet):
@@ -67,11 +68,17 @@ class WatchListView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewse
 
 class InventoryView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (IsAuthenticated,)
-    serializer_class = InventorySerializer
 
     def get_queryset(self, *args, **kwargs):
         queryset = Inventory.objects.filter(user=self.request.user)
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return InventoryDetailSerializer
+        # elif self.action == 'create':
+        #     return InventoryCreateSerializer
+        return InventorySerializer
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
@@ -89,7 +96,8 @@ class PriceView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.G
     queryset = Price.objects.all()
 
 
-class TradeView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet, TradeService):
+class TradeView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.GenericViewSet,
+                TradeService, BaseService):
     permission_classes = (IsAuthenticated,)
     serializer_class = TradeSerializer
 
@@ -97,21 +105,30 @@ class TradeView(ListModelMixin, RetrieveModelMixin, CreateModelMixin, viewsets.G
         queryset = Trade.objects.filter(buyer=self.request.user)
         return queryset
 
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return TradeDetailSerializer
+        # elif self.action == 'create':
+        #     return
+        return TradeSerializer
+
     def create(self, request, *args, **kwargs):
         try:
-            serializer, data = BaseService.get_validate_data_trade(request)
+            serializer, data = self.get_validate_data(request)
+
             buyer_user = data.get('buyer')
             seller_user = data.get('seller')
             buyer_offer = data.get('buyer_offer')
             seller_offer = data.get('seller_offer')
-            # buyer_user, seller_user, buyer_offer, seller_offer, description = data
-            print('buyer', buyer_user, '--', seller_user, buyer_offer, seller_offer)
+            # print('buyer', buyer_user, '--', seller_user, buyer_offer, seller_offer)
 
             # 3
-            user_profile = TradeService.updating_user_score(buyer_user, buyer_offer)
-            inventory_buyer = TradeService.updating_inventory_buyer(buyer_user, buyer_offer)
+            # updating fields buyer
+            TradeService.updating_user_score(buyer_user, buyer_offer)
+            TradeService.updating_inventory_buyer(buyer_user, buyer_offer)
 
             # 4
+            # updating field seller
             seller_profile = TradeService.updating_user_score(seller_user, seller_offer, buyer_offer)
             inventory_seller = TradeService.updating_inventory_seller(seller_user, seller_offer, buyer_offer)
 
